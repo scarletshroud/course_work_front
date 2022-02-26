f64afbceb5b7773958b8cd6dfb1425d23488f42c
Andrey Svistukhin <andrey@enhancera.com>
2022-02-26 18:55:16 +0300

    updated app page

--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -1,52 +1,60 @@
-import React, {useState} from "react";
+import React, {useState} from 'react';
 import {MapContainer, TileLayer, Marker, Popup, useMapEvents} from 'react-leaflet';
 
 export default function MapPage() {
-  let [spots, setSpots] = useState("");
-  const [position, setPosition] = useState(null);
-  
-  const initialPosition = [51.505, -0.09];
-  setPosition(initialPosition);
+  const initPos = [51.505, -0.09];
+  let [spots, setSpots] = useState();
 
-  function getSpotsOnArea(latitude, longitude) {
+  async function updateMarkerList(latitude, longitude) {
     const requestOptions = {
-      method: 'GET',
+      method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({latitude: latitude, longitude: longitude})
     }
-
-    fetch("http://localhost:8080/api/spots", requestOptions)
+    await fetch("http://localhost:8080/api/spots", requestOptions)
       .then(response => response.json())
-      .then(
-        (result) => setSpots(result),
-        (error) => {});
+      .then(result => setSpots(result));
   }
 
-  function MapPositionListener() {
+  function LocationMarker() {
+    let position = null;
     const map = useMapEvents({
-      click() {
-        let currentPosition = map.getCenter()
-        if (currentPosition !== position) {
-          getSpotsOnArea(position.lat, position.lng)
-        }
+      async moveend() {
+        position = map.getCenter();
+        await updateMarkerList(position.latitude, position.longitude);
       }
     })
+
+    return position === null ? null : (
+      <MarkerList spots={spots}/>
+    )
+  }
+
+  function MarkerList(props) {
+    const spots = props.spots;
+    const spotsList = spots.map((marker) =>
+      <Marker key={marker.id} position={[marker.latitude, marker.longitude]}>
+        <Popup>
+          <a href={`http://localhost:8080/api/spot/${marker.id}`}>{marker.name}</a> <br/>
+          {marker.description}
+        </Popup>
+      </Marker>
+    );
+
+    return (
+      <ul>{spotsList}</ul>
+    );
   }
 
   return (
-    <div style={{height: '1000px', width: '1000px'}}>
-      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+    <div>
+      <MapContainer center={initPos} zoom={13} scrollWheelZoom={true} style={{width: '100%', height: '900px'}}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-        <MapPositionListener/>
+        <LocationMarker />
       </MapContainer>
     </div>
-  );
+  )
 }
